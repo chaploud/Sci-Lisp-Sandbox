b1b1a12720811210183804064916f95663c7fe23
Shota Kudo <kudou-s0camfci76w@outlook.com>
2024-02-12 22:39:45 +0900

    tmp

--- src/parser.rs
+++ src/parser.rs
@@ -36,11 +36,150 @@ mod tests {
         "abc\n"
         "##;
 
-        let parsed = pest_parse_to_pairs(code).unwrap().next().unwrap().as_rule();
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::string => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
+        }
+    }
 
-        match parsed {
-            Rule::string => (),
-            _ => panic!("{}", code)
+    #[test]
+    fn test_pest_parse_02_regex() {
+        let code = r##"
+        #"[0-9]+"
+        "##;
+
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::regex => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
+        }
+    }
+
+    #[test]
+    fn test_pest_parse_03_bool() {
+        let code = r##"
+        false
+        true
+        "##;
+
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::bool => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
+        }
+    }
+
+    #[test]
+    fn test_pest_parse_04_nil() {
+        let code = r##"
+        nil
+        "##;
+
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::nil => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
+        }
+    }
+
+    #[test]
+    fn test_pest_parse_05_i64() {
+        let code = r##"
+        -999
+        -1
+        12345
+        +23
+        0
+        0b101
+        0o777
+        0xffa
+        "##;
+
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::i64 => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
+        }
+    }
+
+    #[test]
+    fn test_pest_parse_06_f64() {
+        let code = r##"
+        0.1
+        1.0
+        1.
+        .2
+        -1.
+        -.2
+        3.14e15
+        3.14e-15
+        3.14e+15
+        -3.14e-15
+        -3.e+15
+        -.2e-15
+        nan
+        inf
+        -inf
+        -0.0
+        "##;
+
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::f64 => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
+        }
+    }
+
+    #[test]
+    fn test_pest_parse_07_c64() {
+        let code = r##"
+        0.1j
+        1.0j
+        1.j
+        .2j
+        -1.j
+        -.2j
+        3.14e15j
+        3.14e-15j
+        3.14e+15j
+        -3.14e-15j
+        -3.e+15j
+        -.2e-15j
+        -0.0j
+        -999j
+        -1j
+        12345j
+        +23j
+        1+1j
+        1-1j
+        -1+1j
+        -1-1j
+        0.1+1.0j
+        1.0+1.0j
+        1.+1.0j
+        .2+1.0j
+        -1.+1.0j
+        -.2+1.0j
+        3.14e15+1.0j
+        3.14e-15+1.0j
+        3.14e+15+1.0j
+        -3.14e-15+1.0j
+        -3.e+15+1.0j
+        -.2e-15+1.0j
+        "##;
+
+        for (i, pairs) in pest_parse_to_pairs(code).unwrap().enumerate() {
+            match pairs.as_rule() {
+                Rule::c64 => (),
+                _ => panic!("{0}\n => {1}", code, i)
+            }
         }
     }
 }
