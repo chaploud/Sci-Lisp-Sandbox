ad01e6cbdc359117de65b4c4b3b462ed3cd31628
Shota Kudo <kudou-s0camfci76w@outlook.com>
2024-02-12 19:48:26 +0900

    add test

--- src/parser.rs
+++ src/parser.rs
@@ -6,27 +6,41 @@ use pest::Parser as ParserTrait;
 use pest_derive::Parser;
 
 use crate::structures::Result;
-use crate::structures::errors::Error;
+use crate::structures::errors::Error::*;
 
 #[derive(Parser)]
 #[grammar = "parser/grammar.pest"]
 pub struct Parser;
 
 pub fn parse(code: &str) {
-    let result = Parser::parse(Rule::scilisp, code);
-    match result {
-        Ok(pairs) => {
-            println!("{:#?}", pairs);
-        }
-        Err(e) => {
-            println!("{:#?}", e);
-        }
-    }
+    println!("{:?}", pest_parse_to_pairs(code));
 }
 
-pub fn pest_parse(code: &str) -> Result<Pairs<Rule>> {
-    match Parser::parse(Rule::scilisp, code) {
-        Ok(pairs) => Ok(pairs),
-        Err(e) => Err(Error::Parse(e)),
+fn pest_parse_to_pairs(code: &str) -> Result<Pairs<Rule>> {
+    let toplevel = Parser::parse(Rule::scilisp, code);
+    let inners = match toplevel {
+        Ok(mut pairs) => pairs.next().unwrap().into_inner().next().unwrap().into_inner(),
+        Err(err) => Err(ParseError(Box::new(err)))?,
+    };
+    Ok(inners)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::pest_parse_to_pairs;
+    use super::Rule;
+
+    #[test]
+    fn test_pest_parse_01_string() {
+        let code = r##"
+        "abc\n"
+        "##;
+
+        let parsed = pest_parse_to_pairs(code).unwrap().next().unwrap().as_rule();
+
+        match parsed {
+            Rule::string => (),
+            _ => panic!("{}", code)
+        }
     }
 }
--- src/structures/errors.rs
+++ src/structures/errors.rs
@@ -3,6 +3,9 @@
 use std::fmt::{self, Debug};
 
 use pest::error::Error as PestError;
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use codespan_reporting::files::SimpleFiles;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 
 use crate::structures::objects::Str;
 use crate::parser::Rule;
@@ -10,14 +13,14 @@ use crate::parser::Rule;
 #[derive(Debug)]
 pub enum Error {
     // wrapped errors
-    Parse(PestError<Rule>),
+    ParseError(Box<PestError<Rule>>),
     // self defined errors
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::Parse(err) => write!(f, "parse error: {}", err),
+            Error::ParseError(err) => write!(f, "ParseError: {}", err),
         }
     }
 }
@@ -25,7 +28,7 @@ impl fmt::Display for Error {
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            Error::Parse(_) => None,
+            Error::ParseError(_) => None,
         }
     }
 }
