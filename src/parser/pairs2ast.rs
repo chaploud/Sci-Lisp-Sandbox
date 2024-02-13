b9bf6696fc345fceccc0fbebdf4b31baa7c3e11b
Shota Kudo <kudou-s0camfci76w@outlook.com>
2024-02-14 06:07:38 +0900

    parse to struct

--- src/parser/pairs2ast.rs
+++ src/parser/pairs2ast.rs
@@ -1,6 +1,9 @@
 
 use pest::iterators::Pairs;
+use pest::iterators::Pair;
 use crate::parser::Rule;
+
+use crate::structures::tokens::TokenKind::*;
 
 pub fn pairs_to_ast(pairs: Pairs<Rule>) {
     for pair in pairs {
@@ -56,8 +59,179 @@ pub fn pairs_to_ast(pairs: Pairs<Rule>) {
             Rule::unquote_splicing => (),
             Rule::splicing => (),
             Rule::type_annotation => (),
+            Rule::right_arrow => (),
+            Rule::auto_gensym => (),
+            Rule::module => (),
+            Rule::member => (),
             Rule::EOI => (),
             _ => (),
         }
     }
 }
+
+fn parse_string(pair: Pair<Rule>) {
+}
+
+fn parse_regex(pair: Pair<Rule>) {
+}
+
+fn parse_i64(pair: Pair<Rule>) {
+}
+
+fn parse_f64(pair: Pair<Rule>) {
+}
+
+fn parse_c64(pair: Pair<Rule>) {
+}
+
+fn parse_bool(pair: Pair<Rule>) {
+}
+
+fn parse_nil(pair: Pair<Rule>) {
+}
+
+fn parse_keyword(pair: Pair<Rule>) {
+}
+
+fn parse_symbol(pair: Pair<Rule>) {
+}
+
+fn parse_list(pair: Pair<Rule>) {
+}
+
+fn parse_vector(pair: Pair<Rule>) {
+}
+
+fn parse_map(pair: Pair<Rule>) {
+}
+
+fn parse_set(pair: Pair<Rule>) {
+}
+
+fn parse_array(pair: Pair<Rule>) {
+}
+
+fn parse_slice(pair: Pair<Rule>) {
+}
+
+fn parse_def_kw(pair: Pair<Rule>) {
+}
+
+fn parse_const_kw(pair: Pair<Rule>) {
+}
+
+fn parse_let_kw(pair: Pair<Rule>) {
+}
+
+fn parse_sete_kw(pair: Pair<Rule>) {
+}
+
+fn parse_defn_kw(pair: Pair<Rule>) {
+}
+
+fn parse_return_kw(pair: Pair<Rule>) {
+}
+
+fn parse_fn_kw(pair: Pair<Rule>) {
+}
+
+fn parse_when_kw(pair: Pair<Rule>) {
+}
+
+fn parse_do_kw(pair: Pair<Rule>) {
+}
+
+fn parse_cond_kw(pair: Pair<Rule>) {
+}
+
+fn parse_switch_kw(pair: Pair<Rule>) {
+}
+
+fn parse_for_kw(pair: Pair<Rule>) {
+}
+
+fn parse_while_kw(pair: Pair<Rule>) {
+}
+
+fn parse_break_kw(pair: Pair<Rule>) {
+}
+
+fn parse_continue_kw(pair: Pair<Rule>) {
+}
+
+fn parse_enum_kw(pair: Pair<Rule>) {
+}
+
+fn parse_struct_kw(pair: Pair<Rule>) {
+}
+
+fn parse_method_kw(pair: Pair<Rule>) {
+}
+
+fn parse_self_kw(pair: Pair<Rule>) {
+}
+
+fn parse_macro_kw(pair: Pair<Rule>) {
+}
+
+fn parse_try_kw(pair: Pair<Rule>) {
+}
+
+fn parse_throw_kw(pair: Pair<Rule>) {
+}
+
+fn parse_catch_kw(pair: Pair<Rule>) {
+}
+
+fn parse_finally_kw(pair: Pair<Rule>) {
+}
+
+fn parse_typedef_kw(pair: Pair<Rule>) {
+}
+
+fn parse_import_kw(pair: Pair<Rule>) {
+}
+
+fn parse_export_kw(pair: Pair<Rule>) {
+}
+
+fn parse_dot(pair: Pair<Rule>) {
+}
+
+fn parse_slash(pair: Pair<Rule>) {
+}
+
+fn parse_and(pair: Pair<Rule>) {
+}
+
+fn parse_quote(pair: Pair<Rule>) {
+}
+
+fn parse_syntax_quote(pair: Pair<Rule>) {
+}
+
+fn parse_unquote(pair: Pair<Rule>) {
+}
+
+fn parse_unquote_splicing(pair: Pair<Rule>) {
+}
+
+fn parse_splicing(pair: Pair<Rule>) {
+}
+
+fn parse_type_annotation(pair: Pair<Rule>) {
+}
+
+fn parse_right_arrow(pair: Pair<Rule>) {
+}
+
+fn parse_auto_gensym(pair: Pair<Rule>) {
+}
+
+fn parse_module(pair: Pair<Rule>) {
+}
+
+fn parse_member(pair: Pair<Rule>) {
+}
+
+
